964e0225bb4cb2450f5ec51a83007e51f067e816
Theoyeah <theodelalot@hotmail.com>
2022-03-30 12:48:55 +0200

    That is pretty nice

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -70,16 +70,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
+		['ur really bad', 0.2], //From 0% to 19%
+		['Trash', 0.4], //From 20% to 39%
+		['it is not hard, you just suck so bad', 0.5], //From 40% to 49%
+		['...', 0.6], //From 50% to 59%
+		['Ok', 0.69], //From 60% to 68%
 		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['Thats pretty nice', 0.8], //From 70% to 79%
+		['Cool!', 0.9], //From 80% to 89%
+		['Sick!!', 1], //From 90% to 99%
+		['INFINITE BOBUX GENERATOR MODE', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	
 	//Code by lemz1
